f35cd374653b6af3a94f5213029d8eebe19c3a9e
Raviranjan Pathak <raviranjanpathak750@gmail.com>
2026-04-30 20:44:46 +0530

    fix cors and api route

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ sequelize.sync().then(() => {
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.onrender.com"
+    "https://store-rating-app-1-t7r5.onrender.com"
   ],
   credentials: true
 }));
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://store-rating-app-u442.onrender.com",
+  baseURL: "https://store-rating-app-u442.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
