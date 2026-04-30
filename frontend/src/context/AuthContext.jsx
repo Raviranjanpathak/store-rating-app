3a384172240319e8333558baa647b291d8d42f35
Raviranjan Pathak <raviranjanpathak750@gmail.com>
2026-04-30 20:36:52 +0530

    fix AuthProvider export

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-export default function AuthProvider({ children }) {
+export function AuthProvider({ children }) {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user"))
   );
